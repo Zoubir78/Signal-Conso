b7e5f384617638f4acd995e050e997765b8421f8
Zoubeir M <44193946+Zoubir78@users.noreply.github.com>
2026-05-09 13:31:56 +0200

    Initial commit

--- src/my_ds_project/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Boilerplate Data Science Package
-
-A template for data science projects with best practices.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Your Name"
-__email__ = "you@example.com"
--- src/my_ds_project/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core functionality for the data science boilerplate.
-"""
-
-# Import your modules here when you add them
-# from .data_processing import DataProcessor
-# from .models import MLModel
-
-# __all__ = ["DataProcessor", "MLModel"]
